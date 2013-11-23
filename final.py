85cf667b20cb9ef8f8dd6ffa22618064504555e2
erinkidd01 <erinkidd01@gmail.com>
2013-11-22 19:45:24 -0800

    Added data files

---
+++ final.py
@@ -0,0 +1,9 @@
+# Erin Coughlan and Evan Casey
+# Machine Learning Final Project
+
+import echonest.remix.audio as audio
+import pyechonest.config as config
+import sklearn
+
+config.ECHO_NEST_API_KEY = "U98ZZRHBZWNWUDKPW"
+data_file = kaggle_visible_evaluation_triplets.txt
