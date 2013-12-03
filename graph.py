f7afa7b96b2d58f3f4d2589d159e9514cb86bcb9
erinkidd01 <erinkidd01@gmail.com>
2013-12-02 20:38:45 -0800

    Finished graphing algorithm

--- graph.py
+++ graph.py
@@ -22,6 +22,7 @@ data_users = "data/kaggle_users.txt"
 data_song_track = "data/taste_profile_song_to_tracks.txt"
 data_subset_song_track = "data/subset_unique_tracks.txt"
 data_analysed_songs = "data/analyzed_data_subset.csv"
+#data_analysed_songs = "data/analyzed_data_active_subset.txt"
 
 kmeans_predicted_results_file = "data/results/kmeans_predict.csv"
 test_results_file = "data/results/kmeans_test.csv"
@@ -61,10 +62,6 @@ if __name__ == '__main__':
     # grab user, song data
     user_song_history, songCount = getData()
 
-    # truncate our song data, only run once to generate txt
-    # utils.getSmallerSubset(user_song_history,1000)
-    # utils.getActiveUserSubset(user_song_history,1000)
-
     # grab our song data from echonest
     songDataFull = getAnalyzedData()
     songDataFull = [item[:-3] for item in songDataFull]
@@ -81,49 +78,56 @@ if __name__ == '__main__':
     # process the datasets in case of categorical values
     songData, _ = utils.process(songData)
     
-    # for kmeans, take out a random 50% of songs from each user_song_history
-    user_song_history_test = {}
-    for user,song_list in user_song_history_subset.items():
-        user_song_history_test[user] = random.sample(song_list, len(song_list)/2)
-
-    # take out user_song_history_test items from user_song_history
-    user_song_history_train = {}
-    for user,song_list in user_song_history_test.items():
-        user_song_history_train[user] = [song for song in user_song_history_subset[user] if song not in user_song_history_test[user]]
-
-
-    songData = np.asarray(songData).astype(np.float)
-    songDataNorm = preprocessing.normalize(songData).tolist()
-    songDataFullNorm = [[songDataFull[i][0]] + songDataNorm[i] for i in range(len(songDataFull))]
-
-    ###### Kmeans ######
-
-    test_results = []
-    f = open(test_results_file, 'rt')
-    reader = csv.reader(f)
-    for row in reader:
-        user = row[0]
-        songs = row[1:]
-        test_results.append(songs)
 
     # Train and graph using different number of clusters
-    clusters = [25,50,75,100,125,150,175,200,225,250,275,300,325,350,375,400,425,450,475,500]
+    clusters = [1,10,20,30,40,50,75,100,125,150,175,200,250,300,350,400,500]
     results = []
+    trials = 2
     for c in clusters:
-        # get our giant kmeans learner
-        kmeans_learner = item_kmeans.trainKmeans(songDataNorm, c)  
-        item_kmeans.testKmeans(kmeans_learner, songDataFullNorm, user_song_history_test, user_song_history_train)
+        sumR = 0
+        for i in range(trials):
+            # for kmeans, take out a random 50% of songs from each user_song_history
+            user_song_history_test = {}
+            for user,song_list in user_song_history_subset.items():
+                user_song_history_test[user] = random.sample(song_list, len(song_list)/2)
 
-        kmeans_predicted_results = []
-        f = open(kmeans_predicted_results_file, 'rt')
-        reader = csv.reader(f)
-        for row in reader:
-            user = row[0]
-            songs = row[1:]
-            kmeans_predicted_results.append(songs)
+            # take out user_song_history_test items from user_song_history
+            user_song_history_train = {}
+            for user,song_list in user_song_history_test.items():
+                user_song_history_train[user] = [song for song in user_song_history_subset[user] if song not in user_song_history_test[user]]
+
+
+            songData = np.asarray(songData).astype(np.float)
+            songDataNorm = preprocessing.normalize(songData).tolist()
+            songDataFullNorm = [[songDataFull[i][0]] + songDataNorm[i] for i in range(len(songDataFull))]
+
+            ###### Kmeans ######
+
+            # get our giant kmeans learner
+            kmeans_learner = item_kmeans.trainKmeans(songDataNorm, c)  
+            item_kmeans.testKmeans(kmeans_learner, songDataFullNorm, user_song_history_test, user_song_history_train)
+
+            kmeans_predicted_results = []
+            f = open(kmeans_predicted_results_file, 'rt')
+            reader = csv.reader(f)
+            for row in reader:
+                user = row[0]
+                songs = row[1:]
+                kmeans_predicted_results.append(songs)
+
+            test_results = []
+            f = open(test_results_file, 'rt')
+            reader = csv.reader(f)
+            for row in reader:
+                user = row[0]
+                songs = row[1:]
+                test_results.append(songs)
+            
+            sumR += map.kdd_mapk(test_results, kmeans_predicted_results, 500)
         
-        sumR = map.kdd_mapk(test_results, kmeans_predicted_results, 500)
-        results.append(sumR)
+        avg = sumR / (trials*1.0)
+        results.append(avg)
+        print c, avg
 
     print clusters
     print results
